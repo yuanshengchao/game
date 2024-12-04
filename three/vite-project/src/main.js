d55d8b82277336a69e8c60ef8b4e1d0b0d03fa0e
avatar <>
2024-12-04 16:12:35 +0800

    feat:全局挂在axios

--- three/vite-project/src/main.js
+++ three/vite-project/src/main.js
@@ -4,8 +4,11 @@ import "./assets/css/tailwindcss.css"
 import 'element-ui/lib/theme-chalk/index.css';
 import router from "./router/index.js"
 import App from './App.vue';
+// 全局挂在axios
+import {request} from "@/lib/axios/request.js"
 
 Vue.use(ElementUI);
+Vue.prototype.$axios = request;
 
 new Vue({
  router,
